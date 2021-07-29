3f32b56d0d9a70286151dcdb69d706770201659a
Matthew Oro <MatthewEOro@gmail.com>
2021-07-29 00:25:41 -0400

    reduce DOM use on LandingPage.js

--- client/src/components/VideoPlayer/VideoPlayer.js
+++ client/src/components/VideoPlayer/VideoPlayer.js
@@ -4,11 +4,10 @@ import { ApiKey } from '../../apiKey'
 export default function VideoPlayer({data,match,resetVideoRef}){
 	const apiVideo = data.video + '/' + ApiKey
 	let VideoPlayerRef = null;
-	console.log(match)
 	
 	return(
 			<article className='video'>
-				<video  ref={resetVideoRef} className='video__player' controls poster={data.image} id={data.id}>
+				<video  key={match.params.id === data.id? data.id : data.id} className='video__player' controls poster={data.image} id={data.id}>
 					<source className='video__source' src={data.video + '/' + ApiKey} type='video/mp4' />
 				</video>
 			</article>
--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -5,7 +5,6 @@ import VideoSection from '../../components/VideoSection/VideoSection';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
 import VideoList from '../../components/VideoList/VideoList';
 import { Route, Switch } from 'react-router-dom'
-import {  UrlIdConverter } from '../../javascriptHelper';
 import axios from 'axios';
 import { Url,ApiKey,VideoArray} from '../../apiKey';
 
@@ -14,34 +13,33 @@ class Brainflix extends React.Component{
 	state = {
 		video: null,
 		videoList: null,
-		//check if theres errors on inputbox without DOM
 		inputErr: false,
 	}
 
 	componentDidMount = () =>{
-		//Fix Quality
 		axios.get(Url+VideoArray+ApiKey)
 		.then(({data}) =>{
 			this.setState({videoList: data})
-			return axios.get(`${Url + VideoArray + data[0].id}/${ApiKey}`)
+			let filter = data.find(elem => {return elem.id === this.props.match.params.id})
+			return filter ? axios.get(`${Url + VideoArray + filter.id}/${ApiKey}`) : axios.get(`${Url + VideoArray + data[0].id}/${ApiKey}`)
 		})
 		.then(({data}) =>{
 			this.setState({video: data})
 		})
-		.catch(error =>{
-			console.log(error)
+		.catch(resolve =>{
+			console.log(resolve)
 		})
-		//==========================================================================
-
 	}
 
-	componentDidUpdate = ({match} , prevState) => {
-			//Fix Quality
-			if(this.props.match.url !== match.url){
-				if(this.props.url === '/'){
+	componentDidUpdate = ({match}) => {
+			if(this.props.match.params.id !== match.params.id){
+				if(this.props.match.url === '/'){
 					axios.get(`${Url + VideoArray + this.state.videoList[0].id}/${ApiKey}`)
 					.then(({data}) =>{
 						this.setState({video: data})
+					})
+					.catch(resolve =>{
+						console.log(resolve)
 					})
 				}
 				else{
@@ -49,46 +47,31 @@ class Brainflix extends React.Component{
 					.then(({data}) =>{
 						this.setState({video: data})
 					})
+					.catch(resolve =>{
+						console.log(resolve)
+					})
 				}
-				console.log('Leak test')
 				window.scrollTo(0, 0)
-				console.log(this.resetVideos,'inside')
 			}
-
-			//====================================================================================
-			
 	}
 	handleDelete = (event) =>{
-		//NO DOM
-		const getId = UrlIdConverter(this.props.match.url,this.state.videoList)
 		event.preventDefault();
-		const test = document.querySelectorAll('.comment__posted-wrapper')
 		const commentId = event.target.firstChild.id
 		axios.delete(Url+ VideoArray + this.state.video.id + '/comments/' + commentId + '/' + ApiKey)
 		.then(response =>{
-			console.log(response)
-			axios.get(Url+ VideoArray + getId + '/' + ApiKey)
-			.then(response2 =>{
-				this.setState({video: response2.data})
+			return axios.get(Url+ VideoArray + this.state.video.id + '/' + ApiKey)
+			.then(({data}) =>{
+				this.setState({video: data})
 			})
 		})
 		.catch(resolve =>{
 			console.log(resolve)
 		})
-		setTimeout(()=>{
-			test[test.length-2].scrollIntoView()
-		},150)
-
 
 	}
 handleOnSubmit = (event) =>{
-	//NO DOM
 	event.preventDefault();
 	const form = event.target
-	let eventObj = {
-		name: 'Mohan Muruge',
-		comment: form.postComment.value
-	}
 	if(!form.postComment.value){
 			this.setState({inputErr: true})
 		setTimeout(() =>{
@@ -96,36 +79,22 @@ handleOnSubmit = (event) =>{
 		},1000)
 	}
 	else{
+		let eventObj = {
+			name: 'Mohan Muruge',
+			comment: form.postComment.value
+		}
 		axios.post(Url+ VideoArray + this.state.video.id + '/comments/' + ApiKey,eventObj)
 		.then(response =>{
-			console.log(response)
 			return axios.get(Url+ VideoArray + this.state.video.id + '/' + ApiKey)
 		})
 		.then(({data}) =>{
 			this.setState({video: data})
 		})
-		form.postComment.value = null;
-	}
-	{/*
-	else{
-		axios.post(Url+ VideoArray + this.state.video.id + '/comments/' + ApiKey,eventObj)
-		.then(response =>{
-			console.log(response)
-			axios.get(Url+ VideoArray + getId + '/' + ApiKey)
-			.then(response2 =>{
-				console.log(response2)
-				this.setState({video: response2.data})
-			})
-		})
 		.catch(resolve =>{
 			console.log(resolve)
 		})
-		setTimeout(()=>{
-			test[test.length-1].scrollIntoView()
-		},150)
 		form.postComment.value = null;
 	}
-	*/}
 }
 
 
@@ -136,7 +105,7 @@ handleOnSubmit = (event) =>{
 			<>
 				<Route key='uniqueKey' path='/' component={Header}  />
 				<Route key='uniqueKey1'  path='/' render ={(routerProps)=>
-				<VideoPlayer key={this.state.curentId} resetVideoRef={(video)=>{this.resetVideos = video}}  data={this.state.video} {...routerProps} />
+				<VideoPlayer key={this.state.curentId} data={this.state.video} {...routerProps} />
 					} />
 				<div className='component'>
 					<VideoSection key={this.state.curentId}  checkErr={this.state.inputErr} data={this.state.video} handler={this.handleOnSubmit} deleteClick={this.handleDelete} />
